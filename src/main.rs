27c3e64668b9dc0d97126ddaca94d36047d4837c
FiretreePvP <dominikjoschua@gmail.com>
2026-04-19 23:40:41 +0200

    Einfache I/O Wörter vergleich

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use random_word::Lang;
+
 fn main() {
-    println!("Hello, world!");
+    let word = random_word::get(Lang::En);
+    println!("{}", word);
+
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("Error while reading");
+    let input = input.trim();
+
+    if input != word {
+        println!("'{}' is wrong", input);
+    }
+    else{
+        println!("'{}' is correct", input);   
+    }
 }
