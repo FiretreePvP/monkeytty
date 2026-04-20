3a880b5618912418513c2ad33da9cf5669652a47
FiretreePvP <dominikjoschua@gmail.com>
2026-04-20 23:35:29 +0200

    Outsourced code to methods and made result text colored

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,35 @@
 use random_word::Lang;
+use colored::Colorize;
 
 fn main() {
-    let sentence = generate_sentence(5);
+    let sentence = generate_sentence(2);
     println!("{}", sentence);
 
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).expect("Error while reading");
-    let input = input.trim_end_matches(&['\r', '\n'][..]);
+    let input = get_input();
+    let result = input_correct(input, sentence);
 
-    if input != sentence {
-        println!("'{}' is wrong", input);
-    }
-    else{
-        println!("'{}' is correct", input);
-    }
+    println!("{}", if result {"Correct!".green()} else {"Incorrect!".red()})
 }
 
-fn generate_sentence(length: i32) -> String{
+fn generate_sentence(length: i32) -> String {
     let mut sentence = String::new();
-    for _i in 1..length + 1{
+    for _i in 1..length + 1 {
         let word = random_word::get(Lang::En);
         sentence = sentence + " " + word;
     }
     
     return sentence.trim_start_matches(' ').to_string();
+}
+
+fn input_correct(input: String, sentence: String) -> bool {
+    if input != sentence {
+        return false;
+    }
+    return true;
+}
+
+fn get_input() -> String {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("Error while reading");
+    return input.trim_end_matches(&['\r', '\n'][..]).to_string();
 }
