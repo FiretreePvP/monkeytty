5857d9b5657595c05c0abddf68e5e08ecea0447f
FiretreePvP <dominikjoschua@gmail.com>
2026-04-27 23:51:02 +0200

    Added Ratatui tutorial code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 mod core;
 mod user_interface;
 
+use user_interface::ratatui as ui;
+
+/*
 use user_interface::terminal as ui;
+
 
 fn main() {
     let sentence = core::generate_sentence(10);
@@ -9,4 +13,11 @@ fn main() {
     
     let input = ui::get_input();
     ui::write_sentence_with_highlighting(core::split_sentence(sentence), core::split_sentence(input));
+}
+
+*/
+
+fn main() {
+    let sentence = core::generate_sentence(10);
+    ui::init_console().expect("Error while reading");
 }
